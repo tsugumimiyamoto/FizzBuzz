b1922581ce0059f0ce9354cb04b46b8c24e269d6
tsugumimiyamoto <miyamoto@route-zero.com>
2019-02-12 21:48:52 +0900

    indent hennkou

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -10,11 +10,12 @@ public class FizzBuzz {
 			System.out.println("FizzBuzz");
 			}else if(i%3==0) {
 				System.out.println("Fizz");
-				}else if(i%5==0) {
-					System.out.println("Buzz");
-				}else {
-					System.out.println(i);
-				}
+			}else if(i%5==0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(i);
+			}
 		}
 	}
 }
+
