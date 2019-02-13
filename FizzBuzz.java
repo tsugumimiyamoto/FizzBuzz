8483377dfcaf7244854061a657e3e249ca7bd475
tsugumimiyamoto <miyamoto@route-zero.com>
2019-02-13 16:56:11 +0900

    indent hennkou

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,17 +5,15 @@ public class FizzBuzz {
 		System.out.println("数字を入力。");
 		int a=sc.nextInt();
 		for(int i=1;i<=a;i++){
-
-		if(i%3==0 && i%5==0) {
-			System.out.println("FizzBuzz");
-		}else if(i%3==0) {
-			System.out.println("Fizz");
-		}else if(i%5==0) {
-			System.out.println("Buzz");
-		}else {
-			System.out.println(i);
-		}
+			if(i%3==0 && i%5==0) {
+				System.out.println("FizzBuzz");
+			}else if(i%3==0) {
+				System.out.println("Fizz");
+			}else if(i%5==0) {
+				System.out.println("Buzz");
+			}else {
+				System.out.println(i);
+			}
 		}
 	}
 }
-
